b7999d986424cc05b81d47e59d58f91e47a5cea1
OlehKMS <olegvodafons@gmail.com>
2022-08-25 09:01:01 +0200

    Begin content

--- JS/home_page.js
+++ JS/home_page.js
@@ -3,12 +3,28 @@ let hamburger = document.getElementById('menu__toggle')
 hamburger.onclick = function () {
    
    hamburger.classList.toggle('hamburger_list')
-}
+};
 
 let sets = document.getElementById('sets')
+let sets_block = document.getElementById('sets_block')
 
 sets.onclick = function () {
    
-   sets.classList.toggle('onclick_sets')
+   sets_block.classList.toggle('onclick_sets')
+}
+
+let eticet = document.getElementById('eticets')
+let eticet_block = document.getElementById('eticet_block')
+
+eticet.onclick = function () {
+   eticet_block.classList.toggle('onclick_sets')
+}
+
+let list_wall = document.getElementById('list_wall')
+let wall_block = document.getElementById('eticet_wall')
+
+list_wall.onclick = function () {
+
+   wall_block.classList.toggle('onclick_sets')
 }
   
